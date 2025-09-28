8254f3f4eba8024f757d89bae4457c847b054470
tylerbouldin16 <tylerbouldin16@gmail.com>
2025-09-28 19:13:46 -0400

    updating website

--- csce242/excersises/04_js/js/loops.js
+++ csce242/excersises/04_js/js/loops.js
@@ -9,4 +9,20 @@ document.getElementById('btn-display').onclick = () => {
         li.innerHTML = `${i}`;
         ul.appendChild(li);
     }
-}
+};
+
+
+document.getElementById('btn-count').onclick = () => {
+    const error = document.getElementById('error-number');
+    error.innerHTML = "";
+
+    const startNum = parseInt(document.getElementById('txt-start').value);
+    const endNum = parseInt(document.getElementById('txt-end').value);
+
+    const resultDiv = document.getElementById('d-count');
+    resultDiv.innerHTML = "";
+
+    if(startNum > endNum){
+        error.innerHTML = "Start number must be less than end number";
+    }
+};
